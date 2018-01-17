483636b6a1a0d5d6b9fa5a054d248e669a0b5094
Nhatz HK <nhatz.info@gmail.com>
2018-01-17 08:04:13 -0500

    Updated the constants defining the scene 1. viewport 2. scene dimension 3. path to background alias

--- core/src/main/java/com/bdeb1/unfaithful/util/Constants.java
+++ core/src/main/java/com/bdeb1/unfaithful/util/Constants.java
@@ -1,17 +1,27 @@
 package com.bdeb1.unfaithful.util;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector3;
+
 public final class Constants {
 
 	public static final class Path {
 
 		public static final String BACKGROUND = "assets/image/background.png";
+		public static final String BACKGROUND_ATLAS =
+			  "assets/atlas/Background_lvl_1.atlas";
 	}
 
 	public static final class World {
 
 		public static final Dimension SCENE_DIMENSION = new Dimension (640,
 		                                                               180);
-		public static final float     SCENE_PARTITION = 1 / 3f;
+		public static final Dimension VIEW_DIMENSION  = new Dimension (
+			  Gdx.graphics.getWidth (), Gdx.graphics.getHeight ());
+		public static final Vector3   SCENE_ORIGIN    = new Vector3 (
+			  VIEW_DIMENSION.width / 2, VIEW_DIMENSION.height / 2, 0);
+		public static final Vector3   SCENE_ANCHOR    = new Vector3 (
+			  SCENE_ORIGIN.x + SCENE_DIMENSION.width / 4, SCENE_ORIGIN.y, 0);
 		public static final float     CAMERA_PAN_EASE = 0.05f;
 	}
 }
