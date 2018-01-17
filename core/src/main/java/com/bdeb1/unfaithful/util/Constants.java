340cb50e9066b5b7786e46f5955b5a89612e0ea7
Nhatz HK <nhatz.info@gmail.com>
2018-01-17 01:27:58 -0500

    Added Constants management utility class

---
+++ core/src/main/java/com/bdeb1/unfaithful/util/Constants.java
@@ -0,0 +1,17 @@
+package com.bdeb1.unfaithful.util;
+
+public final class Constants {
+
+	public static final class Path {
+
+		public static final String BACKGROUND = "assets/tmp/background.png";
+	}
+
+	public static final class World {
+
+		public static final Dimension SCENE_DIMENSION = new Dimension (1920,
+		                                                               180);
+		public static final float     SCENE_PARTITION = 1 / 3f;
+		public static final float     CAMERA_PAN_EASE = 0.05f;
+	}
+}
