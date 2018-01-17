0b7d5231f802319f94f634c2bc8b592d46dc2324
Nhatz HK <nhatz.info@gmail.com>
2018-01-17 02:58:36 -0500

    Fixed path

--- core/src/main/java/com/bdeb1/unfaithful/util/Constants.java
+++ core/src/main/java/com/bdeb1/unfaithful/util/Constants.java
@@ -4,12 +4,12 @@ public final class Constants {
 
 	public static final class Path {
 
-		public static final String BACKGROUND = "assets/tmp/background.png";
+		public static final String BACKGROUND = "assets/image/background.png";
 	}
 
 	public static final class World {
 
-		public static final Dimension SCENE_DIMENSION = new Dimension (1920,
+		public static final Dimension SCENE_DIMENSION = new Dimension (640,
 		                                                               180);
 		public static final float     SCENE_PARTITION = 1 / 3f;
 		public static final float     CAMERA_PAN_EASE = 0.05f;
